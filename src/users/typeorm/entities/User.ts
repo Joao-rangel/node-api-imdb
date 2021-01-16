87235a1944217fe73e0749de6039764b2b60191c
Joao Rangel <joao.vran@gmail.com>
2021-01-15 21:00:20 -0300

    create users model

---
+++ src/users/typeorm/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  admin: boolean;
+
+  @Column()
+  date: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
+
+export default User;
