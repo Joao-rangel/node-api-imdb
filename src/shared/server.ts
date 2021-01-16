f07a57b1285dc64487a080e83747cd4902e2cf4d
Joao Rangel <joao.vran@gmail.com>
2021-01-16 09:13:58 -0300

    create user routes

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,8 +1,16 @@
+import 'reflect-metadata';
+
 import express from 'express';
+
+import routes from './routes';
+
+import './typeorm';
+import './container';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
