c28f4dca0fe301e3f0c4a893770d2ad825a07842
Joao Rangel <joao.vran@gmail.com>
2021-01-17 20:12:02 -0300

    add AppError handler

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
+import AppError from './errors/AppError';
 import routes from './routes';
 
 import './typeorm';
@@ -11,6 +13,24 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err); // eslint-disable-line no-console
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
